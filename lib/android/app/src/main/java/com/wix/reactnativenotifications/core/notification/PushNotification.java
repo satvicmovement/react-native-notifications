c6e0f723b0ee59696068605eeb57aacda453c732
satvicmovement <151542201+satvicmovement@users.noreply.github.com>
2024-11-26 16:01:39 +0530

    show toast in foreground too

--- lib/android/app/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/core/notification/PushNotification.java
@@ -64,8 +64,8 @@ public class PushNotification implements IPushNotification {
 
     @Override
     public void onReceived() throws InvalidNotificationException {
+        postNotification(null);
         if (!mAppLifecycleFacade.isAppVisible()) {
-            postNotification(null);
             notifyReceivedBackgroundToJS();
         } else {
             notifyReceivedToJS();
